33e16e93a070908c587e371c31a08994a655926b
Nekdash <78729514+Nekdash@users.noreply.github.com>
2021-10-11 20:32:11 +0300

    Update itc_bin_num.cpp

--- itc_bin_num.cpp
+++ itc_bin_num.cpp
@@ -1,11 +1,11 @@
 #include "middle.h"
 
 long long itc_bin_num(long long number){
-    if ( number > ipow(2, 63) - 1 || number < ipow(2, 63) *(-1))
-        return -1;
+    if (number < 0){
+        return (itc_bin_num(number * -1))* -1;}
     long long res = 0;
     int i = 0;
-    while (number != 0){
+    while (number > 0){
         res += (number % 2)* ipow(10, i);
         i++;
         number = number / 2;
