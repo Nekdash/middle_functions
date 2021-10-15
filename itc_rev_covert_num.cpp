fc6c80b9e1c061a130b82c70eaa703af6e4f715c
Nekdash <78729514+Nekdash@users.noreply.github.com>
2021-10-15 10:09:10 +0300

    Update itc_rev_covert_num.cpp

--- itc_rev_covert_num.cpp
+++ itc_rev_covert_num.cpp
@@ -2,6 +2,9 @@
 
  int itc_rev_covert_num(long long number, int ss){
     long long res = 0;
+  if ( number < 0){
+   return itc_covert_num(number *(-1), ss)*(-1);
+   }
     int len = itc_len_num(number);
     for (int i =0; i < len; i++){
         res += (number%10)*ipow(ss, i);
