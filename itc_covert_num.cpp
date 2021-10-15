b7b2d46f755ef8f663fd2299c161a9e04971d061
Nekdash <78729514+Nekdash@users.noreply.github.com>
2021-10-15 10:08:42 +0300

    Update itc_covert_num.cpp

--- itc_covert_num.cpp
+++ itc_covert_num.cpp
@@ -1,11 +1,13 @@
 #include "middle.h"
 
  int itc_covert_num(long long number, int ss){
-    if ( number > ipow(2, 63) - 1 || number < ipow(2, 63) *(-1))
-        return -1;
+    
     long long res = 0;
     int i = 0;
-    while (number != 0){
+  if ( number < 0){
+   return itc_covert_num(number *(-1), ss)*(-1);
+   }
+      while (number != 0){
         res += (number % ss)*ipow(10, i);
         i++;
         number = number / ss;
